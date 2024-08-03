78cb4b40a0841f0d043c689286a5443a32752812
mohan <49581608+themohan@users.noreply.github.com>
2024-08-03 15:23:49 +0530

    serializers

--- automatic_study_planner/api/serializers.py
+++ automatic_study_planner/api/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from automatic_study_planner.planner.models import StudentInfo, SubjectInfo, TimeTableGenerated
+
+
+class StudentInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentInfo
+        fields = "__all__"
+
+
+class SubjectInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubjectInfo
+        fields = "__all__"
+
+
+class TimeTableGeneratedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TimeTableGenerated
+        fields = "__all__"
