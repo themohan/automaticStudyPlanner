905e42ead5141b9e34095d338d55352359990b03
mohan <49581608+themohan@users.noreply.github.com>
2024-08-03 16:40:19 +0530

    admin register

--- automatic_study_planner/planner/admin.py
+++ automatic_study_planner/planner/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(StudentInfo)
+class StudentInfoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SubjectInfo)
+class SubjectInfoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TimeTableGenerated)
+class TimeTableGeneratedAdmin(admin.ModelAdmin):
+    pass
