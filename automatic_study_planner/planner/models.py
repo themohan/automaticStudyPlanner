940fc43cea14847fad0977aa2e721bb885df7d0f
mohan <49581608+themohan@users.noreply.github.com>
2024-08-03 15:19:23 +0530

    models setup

--- automatic_study_planner/planner/models.py
+++ automatic_study_planner/planner/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class StudentInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    no_of_subjects = models.CharField(max_length=255)
+    no_of_days = models.IntegerField()
+    no_of_hours = models.IntegerField()
+
+
+class SubjectInfo(models.Model):
+    name = models.CharField(max_length=255)
+    difficulty_level = models.IntegerField()
+    no_of_chapters = models.IntegerField()
+    student_info_id = models.ForeignKey(StudentInfo, null=False, db_column="form_id", on_delete=models.PROTECT)
+
+
+class TimeTableGenerated(models.Model):
+    day = models.IntegerField()
+    subjects = models.CharField(max_length=500)
+    chapters = models.CharField(max_length=255)
+    student_info_id = models.ForeignKey(StudentInfo, null=False, db_column="form_id", on_delete=models.PROTECT)
