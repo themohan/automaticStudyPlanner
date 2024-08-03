2f41a40075ab5baad91e3bf6d2f386e1bdde3620
sahithi <aendapallysahithireddy@gmail.com>
2024-08-03 15:43:08 +0530

    views

--- automatic_study_planner/api/urls.py
+++ automatic_study_planner/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import create_study_form
 
 urlpatterns = [
+
+    path('create-study-info', create_study_form(), name="create-study-info"),
 
 ]
--- automatic_study_planner/api/views.py
+++ automatic_study_planner/api/views.py
@@ -1,2 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from automatic_study_planner.api.serializers import StudentInfoSerializer
+
+
+@api_view(['POST'])
+def create_study_form(request):
+    item = request.data
+    item = StudentInfoSerializer(data=item)
+    if item.is_valid():
+        item.save()
+    schedule = generate_table(item)
+    return Response(schedule)
+
+def generate_table(item):
+    pass
